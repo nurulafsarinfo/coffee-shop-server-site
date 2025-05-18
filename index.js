925ec8cca97bf14b4c09187a641315c0f8cdeea0
nurulafsarinfo <mdnurulafsar123afsar@gmail.com>
2025-05-18 13:05:09 +0600

    stablished MongoDb database server connection

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=> {
+console.log(process.env.DB_USER)
+
+console.log("DB-USER: ", process.env.DB_USER);
+console.log("DB-PASS: ", process.env.DB_PASS);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pebgaij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log(`You successfully connected with database port on ${port}`)
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
     res.send('Coffee server is getting hotter')
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Coffee server is running port on ${port}`)
 })
